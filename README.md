d60f29c365ea1db781dc9350f16f8e07208519c0
vitbur <vittorioburani@gmail.com>
2022-08-16 19:52:17 +0200

    First commit
      - Add .gitignore
      - Add LICENSE
      - Add README

---
+++ README.md
@@ -0,0 +1,141 @@
+# PyFanta
+## The PyFanta Repository
+
+This bot is an automated Fantasy Football player list generator with statistics for Serie A.
+
+*It will be assumed that have already installed [**Python 3**](https://www.python.org/downloads/) on your PC.*
+
+<hr>
+
+## Requirements Installation:
+### **Windows:**
+*It will be assumed that you cloned **PyFanta** repo at a certain path: ex. "C:/Users/Username/Github/". It will be used the more general "/path/to/" alias to refer to the specific path.*  
+
+Install *python virtualenv*:
+```
+pip install venv
+```
+Create a new python virtual environment inside the holygrail repo (insert its neme in the .gitignore file):
+```
+cd /path/to/pyfanta/
+python -m venv pyenv
+```
+Activate the newly created virtual environment:
+```
+pyenv/Scripts/activate
+```
+Install all python requirements through *pip install*:  
+```
+cd /path/to/pyfanta/
+pip install -r requirements.txt
+```
+Check if there are broken requirements:  
+```
+pip check
+
+> No broken requirements found.
+```
+**If the output is different from the one above**, install the packages listed in the output through *pip install*:
+```
+pip install <missing-module-1> <missing-module-2> <...>
+```
+<br>
+
+### **Ubuntu/Debian Linux:**
+*It will be assumed that you cloned **PyFanta** repo in your home directory.*  
+Install *pip3*:  
+```
+sudo apt update
+sudo apt install python3-pip
+```
+**Optional (Recommended):** Install *python3-venv* for python3 virtual environment management:
+```
+sudo apt install python3-venv
+```
+**Optional (Recommended):** Create a new python3 virtual environment inside the holygrail repo (insert its name in the .gitignore file):
+```
+cd ~/pyfanta
+python3 -m venv pyenv
+```
+**Optional (Recommended):** Activate the newly created virtual environment:
+```
+source pyenv/bin/activate
+```
+**Warning:** if you followed these steps it is possible you will have to call just **"pip"** and not **"pip3"**.
+
+Install all python3 requirements through *pip3 install*:  
+```
+cd ~/pyfanta
+pip3 install -r requirements.txt
+```
+Check if there are broken requirements:  
+```
+pip3 check
+
+> No broken requirements found.
+```
+**If the output is different from the one above**, install the packages listed in the output through *pip3 install*:
+```
+pip3 install <missing-module-1> <missing-module-2> <...>
+```
+<br>
+
+### **MacOS:**
+*It will be assumed that you cloned **holygrail** repo in your home directory.*  
+*It will be assumed that you have already installed **homebrew** on your Mac.*  
+
+*pip3* is installed along with *python3*:  
+```
+brew install python3
+```
+If you have a version of Homebrew prior to 1.5, you may need to launch also:
+```
+brew postinstall python3
+```
+**Optional (Recommended):** Install *python3 virtualenv* for python3 virtual environment management:
+```
+pip3 install virtualenv
+```
+**Optional (Recommended):** Create a new python3 virtual environment inside the holygrail repo (insert its name in the .gitignore file):
+```
+cd ~/pyfanta
+python3 -m venv pyenv
+```
+**Optional (Recommended):** Activate the newly created virtual environment:
+```
+source pyenv/bin/activate
+```
+**Warning:** if you followed these steps it is possible you will have to call just **"pip"** and not **"pip3"**.
+
+Install all python3 requirements through *pip3 install*:  
+```
+cd ~/pyfanta
+pip3 install -r requirements.txt
+```
+Check if there are broken requirements:  
+```
+pip3 check
+
+> No broken requirements found.
+```
+**If the output is different from the one above**, install the packages listed in the output through *pip3 install*:
+```
+pip3 install <missing-module-1> <missing-module-2> <...>
+```
+
+
+<hr>
+
+## Usage:
+### **Windows:**
+Run the main script *"pyfanta.py*":
+```
+cd /path/to/pyfanta
+python holygrail.py
+```
+### **Linux or MacOS:**
+Run the main script *"pyfanta.py*":
+```
+cd ~/pyfanta
+python3 holygrail.py
+```
---
+++ pyfanta.py
@@ -0,0 +1,173 @@
+import pandas as pd
+
+sheet_id = '1-aqSXdI8ErfIp9mEe6K_FZ7HgzLw47-dUVHlzGpIq6k'
+
+sheet_name = 'griglia'
+path = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+griglia_portieri = pd.read_csv(path)
+
+sheet_name = 'vincenti'
+path = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+stima_vincenti = pd.read_csv(path)
+
+sheet_name = 'stats_gazzetta'
+path = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+stats_gazzetta = pd.read_csv(path)
+stats_gazzetta = stats_gazzetta[stats_gazzetta['Partite Giocate'] >= 10]
+
+sheet_name = 'stats_fantacalcio'
+path = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+stats_fantacalcio = pd.read_csv(path)
+stats_fantacalcio = stats_fantacalcio[stats_fantacalcio['Partite Giocate'] >= 10]
+
+squadre = list(stima_vincenti.columns)
+
+for i in range(0,len(squadre)):
+    for j in range(i+1,len(squadre)):
+        if i < j:
+            stima_vincenti[squadre[j]][i] *= -1
+
+
+
+def punti(x):
+    if x == 0:
+        return 2
+    elif x == -2:
+        return 0
+    elif x == -1:
+        return 1
+    elif x == 1:
+        return 4
+    else:
+        return 6
+
+def sorter(v):
+    for i in range(0, len(v)-1):
+        for j in range (i+1, len(v)):
+            if v[i][1] <= v[j][1]:
+                t = v[i]
+                v[i] = v[j]
+                v[j] = t
+    return v
+
+def stima_campionato():
+    classifica = [[squadre[i], 0] for i in range(0, len(squadre))]
+    for i in range(0,len(squadre)):
+        for j in range(0, len(squadre)):
+            classifica[i][1] += punti(-stima_vincenti[squadre[j]][i])
+    return sorter(classifica)
+
+
+
+def posizione(c, s):
+    for i in range(0, len(squadre)):
+        if s == c[i][0]:
+            return i+1
+
+def coppie_portieri():
+    classifica = stima_campionato()
+    coppie = {
+        0: [],
+        1: [],
+        2: [],
+        3: [],
+        4: [],
+        5: []
+    }
+    for i in range(0, len(squadre)):
+        for j in range(i+1, len(squadre)):
+            if griglia_portieri[squadre[i]][j] == 0:
+                coppie[0].append([squadre[i], squadre[j], posizione(classifica, squadre[i]), posizione(classifica, squadre[j])])
+            elif griglia_portieri[squadre[i]][j] == 1:
+                coppie[1].append([squadre[i], squadre[j], posizione(classifica, squadre[i]), posizione(classifica, squadre[j])])
+            elif griglia_portieri[squadre[i]][j] == 2:
+                coppie[2].append([squadre[i], squadre[j], posizione(classifica, squadre[i]), posizione(classifica, squadre[j])])
+            elif griglia_portieri[squadre[i]][j] == 3:
+                coppie[3].append([squadre[i], squadre[j], posizione(classifica, squadre[i]), posizione(classifica, squadre[j])])
+            elif griglia_portieri[squadre[i]][j] == 4:
+                coppie[4].append([squadre[i], squadre[j], posizione(classifica, squadre[i]), posizione(classifica, squadre[j])])
+            elif griglia_portieri[squadre[i]][j] == 5:
+                coppie[5].append([squadre[i], squadre[j], posizione(classifica, squadre[i]), posizione(classifica, squadre[j])])
+    for i in range(0,6):
+        if coppie[i]:
+            popper = []
+            for j in range(0, len(coppie[i])):
+                if coppie[i][j][2]+coppie[i][j][3] > 19:
+                    popper.append(j)
+            for j in range(-len(popper)+1,1):
+                coppie[i].pop(popper[-j])
+    return coppie
+
+
+
+def dataframe_gazzetta():
+    portieri = stats_gazzetta[stats_gazzetta['Ruolo'] == 'P']
+    portieri = portieri.sort_values(by='MFV', ascending=False)
+    portieri.to_excel('fantastats/portieri_gazzetta.xlsx', index=False)
+
+    difensori = stats_gazzetta[stats_gazzetta['Ruolo'] == 'D']
+    difensori = difensori.sort_values(by='MFV', ascending=False)
+    difensori.to_excel('fantastats/difensori_gazzetta.xlsx', index=False)
+
+    centrocampisti = stats_gazzetta[(stats_gazzetta['Ruolo'] == 'C') | (stats_gazzetta['Ruolo'] == 'T (C)')]
+    centrocampisti = centrocampisti.sort_values(by='MFV', ascending=False)
+    centrocampisti.to_excel('fantastats/centrocampisti_gazzetta.xlsx', index=False)
+
+    attaccanti = stats_gazzetta[(stats_gazzetta['Ruolo'] == 'A') | (stats_gazzetta['Ruolo'] == 'T (A)')]
+    attaccanti = attaccanti.sort_values(by='MFV', ascending=False)
+    attaccanti.to_excel('fantastats/attaccanti_gazzetta.xlsx', index=False)
+
+
+def dataframe_fantacalcio():
+    portieri = stats_fantacalcio[stats_fantacalcio['Ruolo'] == 'P']
+    portieri = portieri.sort_values(by='MFV', ascending=False)
+    portieri.to_excel('fantastats/portieri_fantacalcio.xlsx', index=False)
+
+    difensori = stats_fantacalcio[stats_fantacalcio['Ruolo'] == 'D']
+    difensori = difensori.sort_values(by='MFV', ascending=False)
+    difensori.to_excel('fantastats/difensori_fantacalcio.xlsx', index=False)
+
+    centrocampisti = stats_fantacalcio[(stats_fantacalcio['Ruolo'] == 'C') | (stats_fantacalcio['Ruolo'] == 'T (C)')]
+    centrocampisti = centrocampisti.sort_values(by='MFV', ascending=False)
+    centrocampisti.to_excel('fantastats/centrocampisti_fantacalcio.xlsx', index=False)
+
+    attaccanti = stats_fantacalcio[(stats_fantacalcio['Ruolo'] == 'A') | (stats_fantacalcio['Ruolo'] == 'T (A)')]
+    attaccanti = attaccanti.sort_values(by='MFV', ascending=False)
+    attaccanti.to_excel('fantastats/attaccanti_fantacalcio.xlsx', index=False)
+
+
+
+if __name__ == '__main__':
+    classifica = stima_campionato()
+    coppie = coppie_portieri()
+
+    classifica_dict = {
+        'Squadra': [],
+        'Punteggio': []
+    }
+    for i in classifica:
+        classifica_dict["Squadra"].append(i[0])
+        classifica_dict["Punteggio"].append(i[1])
+    classifica = pd.DataFrame(classifica_dict)
+    classifica.to_excel('fantastats/classifica.xlsx', index=False)
+
+    coppie_dict = {
+        'Partite in comune': [],
+        'Squadra 1': [],
+        'Squadra 2': [],
+        'Piazzamento 1': [],
+        'Piazzamento 2': []
+    }
+    for i in range(0,6):
+        if coppie[i]:
+            for j in range(len(coppie[i])):
+                coppie_dict['Partite in comune'].append(i)
+                coppie_dict['Squadra 1'].append(coppie[i][j][0])
+                coppie_dict['Squadra 2'].append(coppie[i][j][1])
+                coppie_dict['Piazzamento 1'].append(coppie[i][j][2])
+                coppie_dict['Piazzamento 2'].append(coppie[i][j][3])
+    coppie = pd.DataFrame(coppie_dict)
+    coppie.to_excel('fantastats/coppie_portieri.xlsx', index=False)
+
+    dataframe_gazzetta()
+    dataframe_fantacalcio()
